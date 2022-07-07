0cbf1ccda36f01ebe58ef466723e08cb0527004a
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2022-07-07 09:27:32 -0300

    Change run_speed

--- MyProyectGodot/Player/Player.gd
+++ MyProyectGodot/Player/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export (int) var run_speed = 200
+export (int) var run_speed = 300
 export (int) var jump_speed = -600
 export (int) var gravity = 1200
 
@@ -12,7 +12,9 @@ func get_input():
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
 	var jump = Input.is_action_just_pressed('ui_up')
-
+	
+	velocity.x += run_speed
+	
 	if jump and is_on_floor():
 		jumping = true
 		velocity.y = jump_speed
@@ -27,3 +29,5 @@ func _physics_process(delta):
 	if jumping and is_on_floor():
 			jumping = false
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+	
+	
