8cfe615cbdf3042f45215d3962c45380f848f97a
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2022-07-08 12:42:13 -0300

    Update Player.gd

--- MyProyectGodot/Player/Player.gd
+++ MyProyectGodot/Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-export (int) var run_speed = 300
+export (int) var run_speed = 400
 export (int) var jump_speed = -600
-export (int) var gravity = 1200
+export (int) var gravity = 1300
 
 var velocity = Vector2()
 var jumping = false
@@ -11,7 +11,7 @@ func get_input():
 	velocity.x = 0
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_just_pressed('ui_up')
+	var jump = Input.is_action_just_pressed("ui_accept")
 	
 	velocity.x += run_speed
 	
@@ -27,7 +27,7 @@ func _physics_process(delta):
 	get_input()
 	velocity.y += gravity * delta
 	if jumping and is_on_floor():
-			jumping = false
+		jumping = false
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 	
 	
