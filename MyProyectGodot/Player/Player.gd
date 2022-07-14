efa52f9a3d6cda0956bdec7a0d74e9dc15532153
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2022-07-14 12:35:12 -0300

    A lot of Changes
    New Player, Coin object, jumpx2

--- MyProyectGodot/Player/Player.gd
+++ MyProyectGodot/Player/Player.gd
@@ -1,47 +1,61 @@
 extends KinematicBody2D
 
+export (int) var gravity = 1300
 export (int) var run_speed = 400
 export (int) var jump_speed = -600
-export (int) var gravity = 1300
-export (StreamTexture) var newTexture
+
+export (StreamTexture) var texture1
+export (StreamTexture) var texture2
+export (StreamTexture) var texture3
 
 var velocity = Vector2()
-var jumping = false
 
+var acelerate = false 
+var jumping = false
+var jumpx2 = false
 
 func player_position():
 	if position.y > 100:
 		position.y = -65
 		position.x = -380
-	
 
 func get_input():
 	velocity.x = 0
-	var right = Input.is_action_pressed('ui_right')
-	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_just_pressed("ui_accept")
-	var change_player = Input.is_action_just_pressed("ui_up")
-	
 	velocity.x += run_speed
 	
-	if jump and is_on_floor():
-		jumping = true
-		velocity.y = jump_speed
-	if right:
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = jump_speed
+			jumping = true
+		else:
+			if jumping and jumpx2:
+				velocity.y = jump_speed
+				jumping = false
+	
+	if Input.is_action_pressed('ui_right') and acelerate:
 		velocity.x += 200
-	if left:
+	if Input.is_action_pressed('ui_left'):
 		velocity.x -= 200
 	
-	if change_player:
-		$Sprite.texture = newTexture
+	if Input.is_action_just_pressed("ui_number_1"):
+		$Sprite.texture = texture1
+		jumpx2 = false
+		acelerate = false
+	if Input.is_action_just_pressed("ui_number_2"):
+		$Sprite.texture = texture2
+		jumpx2 = true
+		acelerate = false
+	if Input.is_action_just_pressed("ui_number_3"):
+		$Sprite.texture = texture3
+		jumpx2 = false
+		acelerate = true
+		
 
 func _physics_process(delta):
 	get_input()
 	player_position()
 	
 	velocity.y += gravity * delta
-	if jumping and is_on_floor():
-		jumping = false
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 	
 	
