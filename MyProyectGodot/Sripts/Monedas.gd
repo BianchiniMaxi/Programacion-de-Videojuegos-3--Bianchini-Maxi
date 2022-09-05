55132feaf68fa08e2944b6dee483586260ed7ca4
Maxi <bianchinimaxibosko@gmail.com>
2022-09-05 11:50:41 -0300

    Update Menus and GUI Monedas

--- MyProyectGodot/Sripts/Monedas.gd
+++ MyProyectGodot/Sripts/Monedas.gd
@@ -3,4 +3,5 @@ extends Area2D
 func _on_Moneda_body_entered(body):
 	if body.is_in_group("Jugador"):
 		get_parent().get_parent().agarrar_moneda()
+		get_parent().get_parent().crear_monedas()
 		queue_free()
--- MyProyectGodot/Sripts/Niveles.gd
+++ MyProyectGodot/Sripts/Niveles.gd
@@ -3,9 +3,12 @@ extends Node2D
 export (String) var siguiente_nivel
 export (String) var nivel_actual
 export (PackedScene) var Corazones
+export (PackedScene) var Monedas
 
 var lista_vidas = []
 var diferencia_corazones = 80
+var diferencia_monedas = 50
+var cantidad_monedas = 0
 var vidas = 3
 var puntos = 0
 
@@ -21,9 +24,9 @@ func _physics_process(delta):
 	get_input()
 	
 	if vidas == 0:
-		$GUI/PopupPanel.popup()
-		$GUI/PopupPanel.get_child(0).get_child(0).visible = false
-		$GUI/PopupPanel.get_child(0).get_child(1).visible = true
+		$"POP UPs"/PopupPanel.popup()
+		$"POP UPs"/PopupPanel.get_child(0).get_child(0).visible = false
+		$"POP UPs"/PopupPanel.get_child(0).get_child(1).visible = true
 		$Jugador.pararJugador()
 	
 
@@ -34,16 +37,24 @@ func get_input():
 
 func _on_Bandera_Final_body_entered(body):
 	if body.is_in_group("Jugador"):
-		$GUI/PopupPanel.popup()
-		$GUI/PopupPanel.get_child(0).get_child(0).visible = true
-		$GUI/PopupPanel.get_child(0).get_child(1).visible = false
+		$"POP UPs"/PopupPanel.popup()
+		$"POP UPs"/PopupPanel.get_child(0).get_child(0).visible = true
+		$"POP UPs"/PopupPanel.get_child(0).get_child(1).visible = false
 		$Jugador.pararJugador()
 		#get_tree().paused = true
+
+func crear_monedas():
+	
+	var nueva_moneda = Monedas.instance()
+	get_tree().get_nodes_in_group("Gui_Monedas")[0].add_child(nueva_moneda)
+	nueva_moneda.global_position.x -= diferencia_monedas * cantidad_monedas
+	cantidad_monedas += 1
+	#lista_vidas.append(nueva_moneda)
 
 func crear_vidas():
 	for i in 3:
 		var nueva_vida = Corazones.instance()
-		get_tree().get_nodes_in_group("Gui")[0].add_child(nueva_vida)
+		get_tree().get_nodes_in_group("Gui_Corazones")[0].add_child(nueva_vida)
 		nueva_vida.global_position.x += diferencia_corazones * i
 		lista_vidas.append(nueva_vida)
 	
@@ -56,7 +67,6 @@ func perder_vidas():
 func _on_Boton_Siguiente_pressed():
 	get_tree().change_scene(siguiente_nivel)
 	
-S
 # Menu si PIERDE
 func _on_Boton_Reiniciar_pressed():
 	get_tree().change_scene(nivel_actual)
