3983b3bdf6b02869c63d16c824b3bee1d4865351
Maxi <bianchinimaxibosko@gmail.com>
2023-02-03 16:48:24 -0300

    Final Background Level 2. Yes bb

--- MyProyectGodot/Sripts/FondoLV2.gd
+++ MyProyectGodot/Sripts/FondoLV2.gd
@@ -7,11 +7,10 @@ export (StreamTexture) var textura1
 export (StreamTexture) var textura2
 export (StreamTexture) var textura3
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	timer = 0
-	fondo = 2
+	fondo = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,17 +18,17 @@ func _process(delta):
 	
 	if timer >= 0.5:
 		
-		if fondo == 1:
-			$Sprite.texture = textura1
+		if fondo == 0:
+			$"/root/Nivel 2/Mapa/ParallaxBackground/ParallaxLayer/Sprite".texture = textura1
+		elif fondo == 1:
+			$"/root/Nivel 2/Mapa/ParallaxBackground/ParallaxLayer/Sprite".texture = textura2
 		elif fondo == 2:
-			$Sprite.texture = textura2
-		elif fondo == 3:
-			$Sprite.texture = textura3
+			$"/root/Nivel 2/Mapa/ParallaxBackground/ParallaxLayer/Sprite".texture = textura3
 		
 		timer = 0
 		fondo += 1
-		if fondo == 4:
-			fondo = 1
+		if fondo == 3:
+			fondo = 0
 		
 	timer += delta
 	
