171904ef5a56414c27a15db6744aacff1ae5a561
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2023-02-04 18:00:09 -0300

    Final Level 2 Backgruond Effect
    Se eliminaron elementos que ya no forman parte del proyecto, se acomodó el efecto del agua en el Nivel 2.

--- MyProyectGodot/Sripts/Fondo.gd
+++ MyProyectGodot/Sripts/Fondo.gd
@@ -1,23 +1,22 @@
 extends Sprite
 
-var timer
-var fondo
-
+#VARIALBES VARIAS EXPORTADAS PARA SSER MODIFICADAS DESDE EL EDITOR
 export (StreamTexture) var textura1
 export (StreamTexture) var textura2
 export (StreamTexture) var textura3
 
-# Called when the node enters the scene tree for the first time.
+#VARIABLES VARIAS
+var timer
+var fondo
+
+#INICIALIZACION DE LAS VARIABLES
 func _ready():
 	timer = 0
 	fondo = 0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+#CAMBIO DE FONDO PARA HACER EFECTO DEL AGUA
 func _process(delta):
-	
 	if timer >= 0.5:
-		
 		if fondo == 0:
 			$"/root/Nivel 2/Mapa/ParallaxBackground/ParallaxLayer/Sprite".texture = textura1
 		elif fondo == 1:
@@ -27,9 +26,9 @@ func _process(delta):
 		
 		timer = 0
 		fondo += 1
+		
 		if fondo == 3:
 			fondo = 0
 		
 	timer += delta
-	
 	
